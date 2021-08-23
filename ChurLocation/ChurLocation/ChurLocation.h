306e790056560e92446eb6ab18296b5e8915fe05
Lai <code._@outlook.com>
2021-08-23 09:34:30 +0800

    first commit

---
+++ ChurLocation/ChurLocation/ChurLocation.h
@@ -0,0 +1,18 @@
+//
+//  ChurLocation.h
+//  ChurLocation
+//
+//  Created by x on 23/8/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ChurLocation.
+FOUNDATION_EXPORT double ChurLocationVersionNumber;
+
+//! Project version string for ChurLocation.
+FOUNDATION_EXPORT const unsigned char ChurLocationVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ChurLocation/PublicHeader.h>
+
+
---
+++ ChurLocation/ChurLocation/ChurLocation.swift
@@ -0,0 +1,14 @@
+//
+//  ChurLocation.swift
+//  ChurLocation
+//
+//  Created by x on 23/8/21.
+//
+
+import Foundation
+
+public class ChurLocation: NSObject {
+    public static func start() {
+        debugPrint("[ChurLocation] Start")
+    }
+}
